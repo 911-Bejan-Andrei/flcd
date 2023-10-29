d180da32736549c72479b02f6a390e918d7e9953
911-Bejan-Andrei <91748223+911-Bejan-Andrei@users.noreply.github.com>
2023-10-29 23:04:41 +0200

    incomplete scanner implementation

---
+++ lab3/src/PIFEntry.java
@@ -0,0 +1,9 @@
+class PIFEntry {
+    int atomCode;
+    int stPos;
+
+    PIFEntry(int atomCode, int stPos) {
+        this.atomCode = atomCode;
+        this.stPos = stPos;
+    }
+}
---
+++ lab3/src/Scanner.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+
+
+
+public class Scanner {
+    SymbolTable st = new SymbolTable();
+    List<PIFEntry> pif = new ArrayList<>();
+
+}
