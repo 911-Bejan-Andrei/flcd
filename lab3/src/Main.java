7b1f4b90c2c47844f5a2280d131e96e0aa39a4e0
911-Bejan-Andrei <91748223+911-Bejan-Andrei@users.noreply.github.com>
2023-11-01 22:14:58 +0200

    fixed strings and implement pif

--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -3,12 +3,12 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner();
-        String p1 = "C:\\Users\\andya\\Desktop\\lab3\\src\\p1";
-        String p2 = "p2.txt";
-        String p3 = "p3.txt";
+        String p1 = "D:\\lab3\\src\\p1";
+        //String p2 = "p2.txt";
+        //String p3 = "p3.txt";
 
         scanner.scan(p1);
-        scanner.scan(p2);
-        scanner.scan(p3);
+        //scanner.scan(p2);
+        //scanner.scan(p3);
     }
 }
--- lab3/src/Scanner.java
+++ lab3/src/Scanner.java
@@ -8,22 +8,23 @@ public class Scanner {
 
     SymbolTable st;
 
-    PIF pif;
+    List<PIFEntry> pif;
 
     public Scanner() {
         this.stPos = 1;
         this.st = new SymbolTable();
+        this.pif = new ArrayList<>();
     }
 
     /*
-    2-13 = separators
-    14-23 = operators
-    24-37 = reserved words
+    0-9 = separators
+    10-21 = operators
+    22-36 = reserved words
     */
     List<String> tokens = readTokens();
-    List<String> separators = readTokensElements(2, 13);
-    List<String> operators = readTokensElements(14, 23);
-    List<String> reservedWords = readTokensElements(24, 37);
+    List<String> separators = readTokensElements(0, 9);
+    List<String> operators = readTokensElements(10, 21);
+    List<String> reservedWords = readTokensElements(22, 36);
 
 
     public void scan(String file) throws IOException {
@@ -35,32 +36,61 @@ public class Scanner {
                 index++;
             }
             tupple = scanToken(index, fileContent);
-            System.out.println(tupple.x + "  " + tupple.y);
-            if(separators.contains(tupple.x) || operators.contains(tupple.x) || reservedWords.contains(tupple.x)) {
-                //PIFEntry pifEntry = new PIFEntry(tokens.indexOf(tupple.x), );
-            } else {
+            if(String.valueOf(tupple.x.charAt(0)).equals("#")) {
+                tupple.x = tupple.x.replace("#", "");
+
+                PIFEntry pifEntry1 = new PIFEntry(tokens.indexOf("#"), -2);
+                pif.add(pifEntry1);
                 TuppleDAO tuppleDAO = new TuppleDAO(tupple.x, tupple.y);
                 st.insert(tuppleDAO);
-                //PIFEntry pifEntry = new PIFEntry();
+                PIFEntry pifEntry2 = new PIFEntry(2, Integer.parseInt(tupple.y));
+                pif.add(pifEntry2);
+                pif.add(pifEntry1);
+
+                tupple.i = String.valueOf(Integer.parseInt(tupple.i) + 1);
+            }
+            else if(separators.contains(tupple.x) || operators.contains(tupple.x) || reservedWords.contains(tupple.x)) {
+                PIFEntry pifEntry = new PIFEntry(tokens.indexOf(tupple.x), Integer.parseInt(tupple.y));
+                pif.add(pifEntry);
+            } else { //this is for identifiers and constants to add in st
+                TuppleDAO tuppleDAO = new TuppleDAO(tupple.x, tupple.y);
+                st.insert(tuppleDAO);
+                PIFEntry pifEntry;
+                if(tupple.x.matches("[0-9]+")) {
+                    pifEntry = new PIFEntry(1, Integer.parseInt(tupple.y));
+                } else {
+                    pifEntry = new PIFEntry(2, Integer.parseInt(tupple.y));
+                }
+                pif.add(pifEntry);
             }
             index = Integer.parseInt(tupple.i) + 1;
             if (String.valueOf(fileContent.charAt(index)).equals("\n")) {
                 index++;
             }
         }
+
+        System.out.println("Symbol table:\n");
+        System.out.println("PIF table:\n");
+        System.out.println("[ac]   [stPos]");
+        for (PIFEntry pifEntry: pif) {
+            System.out.println(pifEntry.atomCode + "        " + pifEntry.stPosition);
+        }
+        System.out.println("done");
     }
 
     public Tupple scanToken(int index, String file) {
         int aux = index;
         String str = "";
 
-        if (aux - 1 > 0 && String.valueOf(file.charAt(aux - 1)).equals("#")) {
-            while (!String.valueOf(file.charAt(aux)).equals("#")) {
+        if(String.valueOf(file.charAt(aux)).equals("#")) {
+            aux++;
+            str += "#";
+            while (aux < file.length() - 1 && !String.valueOf(file.charAt(aux)).equals("#")) {
                 str += file.charAt(aux);
                 aux++;
             }
             return new Tupple(str, String.valueOf(this.stPos++), String.valueOf(aux - 1));
-        } else {
+        }
 
             if (separators.contains(String.valueOf(file.charAt(aux)))) {
                 return new Tupple(String.valueOf(file.charAt(aux)), "-2", String.valueOf(aux));
@@ -70,7 +100,7 @@ public class Scanner {
                 return new Tupple(String.valueOf(file.charAt(aux)), "-1", String.valueOf(aux));
             }
 
-            while (!String.valueOf(file.charAt(aux)).equals(" ") &&
+            while (aux < file.length() && !String.valueOf(file.charAt(aux)).equals(" ") &&
                     !separators.contains(String.valueOf(file.charAt(aux))) &&
                     !operators.contains((String.valueOf(file.charAt(aux))))) {
 
@@ -81,7 +111,6 @@ public class Scanner {
             if (reservedWords.contains(str)) {
                 return new Tupple(str, "0", String.valueOf(aux - 1));
             }
-        }
 
         return new Tupple(str, String.valueOf(this.stPos++), String.valueOf(aux - 1));
     }
@@ -103,7 +132,7 @@ public class Scanner {
 
     public List<String> readTokens() {
         List<String> lines = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\andya\\Desktop\\lab3\\src\\token.in"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("D:\\lab3\\src\\token.in"))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 lines.add(line);
