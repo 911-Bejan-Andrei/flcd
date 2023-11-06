b18320d945d3bc2569d66d8eed5c3abe82173467
911-Bejan-Andrei <91748223+911-Bejan-Andrei@users.noreply.github.com>
2023-11-06 18:22:45 +0200

    add fa

---
+++ lab4/src/FiniteAutomata.java
@@ -0,0 +1,96 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class FiniteAutomata {
+    private Set<String> states;
+    private Set<String> alphabet;
+    private Map<String, Map<String, String>> transitions;
+    private String initialState;
+    private Set<String> finalStates;
+
+    public FiniteAutomata() {
+        states = new HashSet<>();
+        alphabet = new HashSet<>();
+        transitions = new HashMap<>();
+        finalStates = new HashSet<>();
+    }
+
+    public void loadFromFile(String filename) {
+        try {
+            Scanner scanner = new Scanner(new File(filename));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine().trim();
+                if (line.startsWith("States:")) {
+                    states = new HashSet<>(Arrays.asList(line.split(":")[1].split("\\s+")));
+                } else if (line.startsWith("Alphabet:")) {
+                    alphabet = new HashSet<>(Arrays.asList(line.split(":")[1].split("\\s+")));
+                } else if (line.startsWith("Transitions:")) {
+                    while (scanner.hasNextLine()) {
+                        line = scanner.nextLine().trim();
+                        if (line.isEmpty()) {
+                            break;
+                        }
+                        String[] parts = line.split("\\s+");
+                        String source = parts[0];
+                        String symbol = parts[1];
+                        String target = parts[2];
+                        transitions.computeIfAbsent(source, k -> new HashMap<>()).put(symbol, target);
+                    }
+                } else if (line.startsWith("Initial State:")) {
+                    initialState = line.split(":")[1].trim();
+                } else if (line.startsWith("Final States:")) {
+                    finalStates = new HashSet<>(Arrays.asList(line.split(":")[1].split("\\s+")));
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filename);
+        }
+    }
+
+    public void displayElements() {
+        System.out.println("Set of States: " + states);
+        System.out.println("Alphabet: " + alphabet);
+        System.out.println("Transitions:");
+        for (String source : transitions.keySet()) {
+            for (String symbol : transitions.get(source).keySet()) {
+                String target = transitions.get(source).get(symbol);
+                System.out.println(source + " --(" + symbol + ")--> " + target);
+            }
+        }
+        System.out.println("Initial State: " + initialState);
+        System.out.println("Set of Final States: " + finalStates);
+    }
+
+    public boolean isAcceptedSequence(String sequence) {
+        String currentState = initialState;
+        for (char symbol : sequence.toCharArray()) {
+            String symbolStr = String.valueOf(symbol);
+            if (!alphabet.contains(symbolStr)) {
+                return false;
+            }
+            if (transitions.containsKey(currentState) && transitions.get(currentState).containsKey(symbolStr)) {
+                currentState = transitions.get(currentState).get(symbolStr);
+            } else {
+                return false;
+            }
+        }
+        return finalStates.contains(currentState);
+    }
+
+    public static void main(String[] args) {
+        FiniteAutomata fa = new FiniteAutomata();
+        fa.loadFromFile("fa_input.txt");
+        fa.displayElements();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sequence to check if it is accepted by the FA: ");
+        String sequence = scanner.nextLine();
+        if (fa.isAcceptedSequence(sequence)) {
+            System.out.println("Sequence is accepted by the FA.");
+        } else {
+            System.out.println("Sequence is not accepted by the FA.");
+        }
+    }
+}
